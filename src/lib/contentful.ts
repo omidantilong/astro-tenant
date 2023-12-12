575dc4d45e07774b534971aa5ffa182389ebfc94
Omid Kashan <omid@antilong.com>
2023-12-12 23:24:02 +0000

    basic nested pages support

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -1,5 +1,26 @@
 export async function getPage({ slug }: { slug: string }) {
-  const query = `query { pageCollection(where: {slug: "${slug}"}, limit: 1) { items { title } } }`
+  const query = `
+    fragment parentPage on Page {
+      sys {
+        id
+      }
+      title
+      slug
+    }
+    query { 
+      pageCollection(where: {slug: "${slug}"}, limit: 1) { 
+        items { 
+          title,
+          parent {
+            ...parentPage
+          }
+        } 
+      } 
+    }
+  `
+
+  //parentPagequery { blogPostCollection { items { ...blogFields } } } fragment blogFields on BlogPost { title date author { name gitHubUsername } }
+
   const data = await fetch(
     `https://graphql.contentful.com/content/v1/spaces/${
       import.meta.env.CONTENTFUL_SPACE_ID
