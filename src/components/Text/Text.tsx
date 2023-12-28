58b07f592b3c346c1ddb44d703a25d2abfddace5
Omid Kashan <omid@antilong.com>
2023-12-28 07:32:59 +0000

    use unified in Text component
    Using unified with processSync should mean we can use remark plugins more easily, for example to unwrap images or replace the default <img> tag with picture/srcset etc

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,12 +1,22 @@
-import { micromark } from "micromark"
+//import { micromark } from "micromark"
+import { unified } from "unified"
+import remarkParse from "remark-parse"
+import remarkRehype from "remark-rehype"
+import rehypeStringify from "rehype-stringify"
+
+function processMarkdown(text: string) {
+  const processor = unified().use(remarkParse).use(remarkRehype).use(rehypeStringify)
+  const parsed = processor.processSync(text).toString()
+  return parsed
+}
 
 export default function ({ data }: { data: Text }) {
   const { title, text } = data
+  //const parsed = micromark(text)
 
-  const parsed = micromark(text)
+  const parsed = processMarkdown(text)
 
-  // If using react-remark instead, we can just return remarkSync(text)
-  // here instead of using dangerouslySetInnerHTML
+  //return <div>{parsed}</div>
 
   return <div dangerouslySetInnerHTML={{ __html: parsed }} />
 }
