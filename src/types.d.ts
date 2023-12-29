4cf3a83bdfaf72c9fa055b04c1853fbec8370b5b
Omid Kashan <omid@antilong.com>
2023-12-29 13:00:10 +0000

    add EditorialCard component

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,33 +1,3 @@
-declare interface ContentfulPage {
-  slug: string
-  title: string
-  parent: ContentfulPage
+declare interface LinkWrapperProps {
+  href?: string
 }
-
-declare interface ContentfulLegacyPage {
-  url: string
-  title: string
-  parentPage: ContentfulLegacyPage
-}
-
-declare interface Section {
-  type: "Section"
-  title: string
-  contentCollection: {
-    items: ContentComponent[]
-  }
-}
-
-declare interface EditorialCard {
-  type: "EditorialCard"
-  cardHeading: string
-  cardLabel: string
-}
-
-declare interface Text {
-  type: "Text"
-  title: string
-  text: string
-}
-
-declare type ContentComponent = Section | EditorialCard | Text
