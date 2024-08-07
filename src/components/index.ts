5aab3c1ff2cc50722dd95b2a347b8a8a00ca715f
Omid Kashan <omid@antilong.com>
2024-08-07 01:55:57 +0100

    use terser exports for components register

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,17 +1,20 @@
 //import Dummy from "@omidantilong/astro/Dummy"
 //import Foo from "@omidantilong/astro/Foo"
 
-import Dummy from "@omidantilong/astro/Dummy"
-import Foo from "@omidantilong/astro/Foo"
+//import Dummy from "@omidantilong/astro/Dummy"
+// import Foo from "@omidantilong/astro/Foo"
+
+export { Dummy, Foo } from "@omidantilong/astro"
+export { Alert } from "@omidantilong/react"
 
 //import { Foo } from "@omidantilong/astro"
 
 //import { DummyPackage } from "@omidantilong/dummy-astro-package"
 
-import EditorialCard from "@/components/EditorialCard/EditorialCard.astro"
-import Section from "@/components/Section/Section.astro"
-import Text from "@/components/Text/Text.astro"
-import Image from "@/components/Image/Image.astro"
-import Video from "@/components/Video/Video.astro"
+export { default as EditorialCard } from "@/components/EditorialCard/EditorialCard.astro"
+export { default as Section } from "@/components/Section/Section.astro"
+export { default as Text } from "@/components/Text/Text.astro"
+export { default as Image } from "@/components/Image/Image.astro"
+export { default as Video } from "@/components/Video/Video.astro"
 
-export default { Dummy, Foo, EditorialCard, Section, Text, Image, Video }
+//export default { Dummy, Foo, Alert, EditorialCard, Section, Text, Image, Video }
