0340d40154eb00afaade10e90f562e85a3289ac1
Omid Kashan <omid@antilong.com>
2024-12-06 13:22:24 +0000

    chore: update rehype unwrap image plugin

--- src/util/markdown.ts
+++ src/util/markdown.ts
@@ -1,14 +1,14 @@
 import { unified } from "unified"
 import remarkParse from "remark-parse"
 import remarkRehype from "remark-rehype"
-import remarkUnwrapImages from "remark-unwrap-images"
+import rehypeUnwrapImages from "rehype-unwrap-images"
 import rehypeStringify from "rehype-stringify"
 import { rehypeContentfulImage } from "./rehypeContentfulImage"
 
 export async function parse(text: string) {
   const processor = unified()
     .use(remarkParse)
-    .use(remarkUnwrapImages)
+    .use(rehypeUnwrapImages)
     .use(remarkRehype)
     .use(rehypeContentfulImage)
     .use(rehypeStringify)
