48084d3ae45707eda77b1c8af58e00bb0c00bdb0
Omid Kashan <omid@antilong.com>
2024-12-09 22:55:13 +0000

    feat: improve default routes logic
    Prior to Astro 5, Astro's routing logic would silently give priority to injected routes, but would spit out multiple entry points (files) for each match. That logic has changed and route collisions now throw a warning (soon to be an error). To get around this, we can define default routes in a function.

--- astro.config.ts
+++ astro.config.ts
@@ -6,32 +6,47 @@ import type { AstroIntegration } from "astro"
 
 /*
 This is a very simple example of an Astro integration injecting custom
-routes. Astro's route priority always gives higher priority to injected
-routes, when we actually want priority to be more like Wordpress
-template lookup (eg. if it exists in userland, use that instead)
+routes. This is controlled by a config option, customIndex.
 
-However we can work around this with some simple config, or we could
-build some template lookup logic into the integration if that gets
-unmanageable. For now I think it's fine. 
+Setting it to false will use the integration slug route. 
 
-Setting customIndex to false will use the integration slug route. 
-Setting customIndex to true will use the local slug route.
+Setting it to true will use the local slug route.
 
-IRL this engine integration would live inside node_modules. This has no
+NOTE: IRL this engine integration would live inside node_modules. This has no
 bearing on its functionality (in fact this is the exact same approach
-that Starlight uses)
-*/
+that Starlight uses).
 
+NOTE: Prior to Astro 5, Astro's routing logic would silently give
+priority to injected routes, but would spit out multiple entry points
+(files) for each match. That logic has changed and route collisions now
+throw a warning (soon to be an error). To get around this, we can define
+default routes in a function.
+
+In the case where user wants local routes, customIndex is false and the
+function is never called
+
+In the case where user wants default routes, customIndex is true and
+their local [...slug].astro shouldn't exist.
+
+It would be cool if we could set build-time route priority info when
+setting up `injectRoute`. But this seems to work fine for now, and
+correctly outputs just one entry point when creating a build.
+
+
+*/
+const defaultRoute = () => {
+  return {
+    pattern: "[...slug]",
+    entrypoint: "./engine/pages/[...slug].astro",
+  }
+}
 function engine({ ...opts }): AstroIntegration {
   return {
     name: "engine",
     hooks: {
       "astro:config:setup": async ({ injectRoute }) => {
         if (!opts.customIndex) {
-          injectRoute({
-            pattern: "[...slug]",
-            entrypoint: "engine/pages/[...slug].astro",
-          })
+          injectRoute(defaultRoute())
         }
       },
     },
@@ -49,7 +64,7 @@ export default defineConfig({
   }),
   integrations: [
     engine({
-      customIndex: false,
+      customIndex: true,
     }),
     react(),
   ],
