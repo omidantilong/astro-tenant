7176447b3846614c74848bc29c24bbad3bfcd82a
Omid Kashan <omid@antilong.com>
2024-12-14 21:31:52 +0000

    feat: tweak rsc component delay

--- src/components/Text/Rsc.tsx
+++ src/components/Text/Rsc.tsx
@@ -1,6 +1,6 @@
 export default async function Rsc({ text }: { text: string }) {
-  const data = await fetch("https://jsonplaceholder.typicode.com/todos/1").then((response) =>
-    response.json()
+  const data = await fetch("https://jsonplaceholder.typicode.com/todos/1?_delay=2000").then(
+    (response) => response.json()
   )
 
   return (
