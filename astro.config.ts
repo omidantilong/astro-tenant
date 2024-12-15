40e44b8f2f554e715742308987310ebbd140b8dc
Omid Kashan <omid@antilong.com>
2024-12-15 00:15:24 +0000

    feat: move engine integration out of config

--- astro.config.ts
+++ astro.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from "astro/config"
 import { resolve } from "path"
 import node from "@astrojs/node"
 import react from "@astrojs/react"
-import type { AstroIntegration } from "astro"
-import fs from "fs-extra"
 
+import { engine } from "./engine/engine"
 /*
 This is a very simple example of an Astro integration injecting custom
 routes. This is controlled by a config option, customIndex.
@@ -35,51 +34,6 @@ correctly outputs just one entry point when creating a build.
 
 
 */
-const defaultRoute = () => {
-  return {
-    pattern: "[...slug]",
-    entrypoint: "./engine/pages/[...slug].astro",
-  }
-}
-
-interface DefaultRoutes {
-  [key: string]: Function
-}
-
-const defaultRoutes: DefaultRoutes = {
-  "./src/pages/[...slug].astro": () => ({
-    pattern: "[...slug]",
-    entrypoint: "./engine/pages/[...slug].astro",
-  }),
-  "./src/pages/404.astro": () => ({
-    pattern: "404",
-    entrypoint: "./engine/pages/404.astro",
-  }),
-}
-
-function engine({ ...opts }): AstroIntegration {
-  return {
-    name: "engine",
-    hooks: {
-      "astro:config:setup": async ({ injectRoute }) => {
-        // if (!opts.customIndex) {
-        //   injectRoute(defaultRoute())
-        // }
-        // Another way of doing this, without using the customIndex option
-        // Manually test for existence of local template
-        // Could be turned into an array of default template paths to check
-        // if (!fs.existsSync("./src/pages/[...slug].astro")) injectRoute(defaultRoute())
-
-        // And another way, using a defaultRoutes object
-        for (const route in defaultRoutes) {
-          if (!fs.existsSync(route)) injectRoute(defaultRoutes[route]())
-        }
-      },
-    },
-  }
-}
-
-const __dirname = resolve()
 
 export default defineConfig({
   output: "server",
---
+++ engine/engine.ts
@@ -0,0 +1,39 @@
+import type { AstroIntegration } from "astro"
+import fs from "fs-extra"
+
+interface DefaultRoutes {
+  [key: string]: Function
+}
+
+const defaultRoutes: DefaultRoutes = {
+  "./src/pages/[...slug].astro": () => ({
+    pattern: "[...slug]",
+    entrypoint: "./engine/pages/[...slug].astro",
+  }),
+  "./src/pages/404.astro": () => ({
+    pattern: "404",
+    entrypoint: "./engine/pages/404.astro",
+  }),
+}
+
+export function engine({ ...opts }): AstroIntegration {
+  return {
+    name: "engine",
+    hooks: {
+      "astro:config:setup": async ({ injectRoute }) => {
+        // if (!opts.customIndex) {
+        //   injectRoute(defaultRoute())
+        // }
+        // Another way of doing this, without using the customIndex option
+        // Manually test for existence of local template
+        // Could be turned into an array of default template paths to check
+        // if (!fs.existsSync("./src/pages/[...slug].astro")) injectRoute(defaultRoute())
+
+        // And another way, using a defaultRoutes object
+        for (const route in defaultRoutes) {
+          if (!fs.existsSync(route)) injectRoute(defaultRoutes[route]())
+        }
+      },
+    },
+  }
+}
