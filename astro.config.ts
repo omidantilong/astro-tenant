2a9da40bf7f87617bc65d4190f80cb458dfdef83
Omid Kashan <omid@antilong.com>
2024-12-15 20:15:51 +0000

    chore: move engine integration notes

--- astro.config.ts
+++ astro.config.ts
@@ -1,39 +1,9 @@
 import { defineConfig } from "astro/config"
-import { resolve } from "path"
 import node from "@astrojs/node"
 import react from "@astrojs/react"
 
 import { engine } from "./engine/engine"
-/*
-This is a very simple example of an Astro integration injecting custom
-routes. This is controlled by a config option, customIndex.
-
-Setting it to false will use the integration slug route. 
-
-Setting it to true will use the local slug route.
-
-NOTE: IRL this engine integration would live inside node_modules. This has no
-bearing on its functionality (in fact this is the exact same approach
-that Starlight uses).
-
-NOTE: Prior to Astro 5, Astro's routing logic would silently give
-priority to injected routes, but would spit out multiple entry points
-(files) for each match. That logic has changed and route collisions now
-throw a warning (soon to be an error). To get around this, we can define
-default routes in a function.
-
-In the case where user wants local routes, customIndex is false and the
-function is never called
-
-In the case where user wants default routes, customIndex is true and
-their local [...slug].astro shouldn't exist.
-
-It would be cool if we could set build-time route priority info when
-setting up `injectRoute`. But this seems to work fine for now, and
-correctly outputs just one entry point when creating a build.
-
-
-*/
+//import { engineConfig } from "./tenant.config.ts"
 
 export default defineConfig({
   output: "server",
