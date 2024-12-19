b591e5417757f59a074a45a68e38276cf1e65a00
Omid Kashan <omid@antilong.com>
2024-12-19 00:33:24 +0000

    fix: organise package scripts

--- engine/bin/copy-externals.sh
+++ engine/bin/copy-externals.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-mkdir -p ./dist/node_modules/
-cp -r ./node_modules/{react,react-dom,scheduler} ./dist/node_modules/
+mkdir -p dist/node_modules/
+cp -r node_modules/{react,react-dom,scheduler} dist/node_modules/
