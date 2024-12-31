81f03a43aba752a64d56f0a704c50de1c235ab9a
Omid Kashan <omid@antilong.com>
2024-12-31 18:28:06 +0000

    fix: correct page types config

--- tenant.config.ts
+++ tenant.config.ts
@@ -2,7 +2,7 @@ import { defineEngineConfig } from "@omidantilong/engine/config"
 import { PressRelease } from "./tenant/page-types/press-release"
 
 export const engineConfig = defineEngineConfig({
-  pageQueries: {
+  pageTypes: {
     PressRelease,
   },
 })
