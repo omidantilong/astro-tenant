43ffdae088961c44e102b6a32aeaa119a40dac03
Omid Kashan <omid@antilong.com>
2025-01-01 14:00:49 +0000

    fix: remove ambient engine types

--- types/env.d.ts
+++ types/env.d.ts
@@ -1,4 +1,3 @@
 /// <reference types="astro/client" />
-/// <reference types="@omidantilong/engine/types" />
 /// <reference types="@omidantilong/engine/types/cms" />
 /// <reference types="@omidantilong/engine/types/env" />
