a06215f9522eff16c93cb34bc811742b6ebc31e7
Omid Kashan <omid@antilong.com>
2025-01-02 18:15:08 +0000

    feat: migrate to use new engine types

--- types/tenant.d.ts
+++ types/tenant.d.ts
@@ -15,96 +15,95 @@ Suggestions:
 - Combine both sets of fragments like we do with contentTypes
 - Store a default fragment inside each component (in dummy-components etc)?
 */
+import { Sys } from "@omidantilong/engine/types/cms"
 
-declare module "engine:types/cms" {
-  interface PressReleasePage extends Sys {
-    type: "PressRelease"
-    slug: string
-    title: string
-    foo: string
-  }
-  interface LegacyPage extends Sys {
-    type: "Page"
-    slug: string
-    title: string
-    metaTitle: string
-    parent: LegacyPage
-    modulesCollection: { items: Array<TenantContentModule> }
-    heroCollection: { items: Array<Hero> }
-  }
-  interface Hero extends Sys {
-    type: "Hero"
-    title: string
-    heroHeading: string
-    heroBody: string
-    image: ImageProps
-    imageCaption: string
-    link: Link
-  }
-
-  interface Section extends Sys {
-    type: "Section"
-    title: string
-    contentCollection: {
-      items: Array<TenantContentModule>
-    }
-  }
-
-  interface EditorialCard extends Sys {
-    type: "EditorialCard"
-    cardHeading: string
-    cardLabel: string
-    cardBody: string
-    link: Link
-    image: ImageProps
-  }
-
-  interface Text extends Sys {
-    type: "Text"
-    title: string
-    text: string
-  }
-
-  interface InternalLink extends Sys {
-    type: "InternalLink"
-    page: LegacyPage
-    title: string
-    linkStyle: string
-  }
-
-  interface ExternalLink extends Sys {
-    type: "ExternalLink"
-    url: string
-    title: string
-    linkStyle: string
-  }
-  interface Video extends Sys {
-    type: "Video"
-    title: string
-    videoUrl: string
-    transcript: string
-  }
-
-  interface Image extends Sys {
-    type: "Image"
-    title: string
-    image: ImageProps
-  }
-
-  interface ImageProps {
-    url: string
-    title: string
-    description: string
-    height: number
-    width: number
-  }
-
-  type Link = InternalLink | ExternalLink
-
-  //interface TenantPage extends DefaultPage {}
-
-  type TenantPage = LegacyPage | PressReleasePage
-
-  // type Page = DefaultPage | LegacyPage | PressReleasePage
-  type TenantContentModule = Section | EditorialCard | Text | Video | Image
+export interface PressReleasePage extends Sys {
+  type: "PressRelease"
+  slug: string
+  title: string
+  foo: string
 }
+export interface LegacyPage extends Sys {
+  type: "Page"
+  slug: string
+  title: string
+  metaTitle: string
+  parent: LegacyPage
+  modulesCollection: { items: Array<TenantContentModule> }
+  heroCollection: { items: Array<Hero> }
+}
+export interface Hero extends Sys {
+  type: "Hero"
+  title: string
+  heroHeading: string
+  heroBody: string
+  image: ImageProps
+  imageCaption: string
+  link: Link
+}
+
+export interface Section extends Sys {
+  type: "Section"
+  title: string
+  contentCollection: {
+    items: Array<TenantContentModule>
+  }
+}
+
+export interface EditorialCard extends Sys {
+  type: "EditorialCard"
+  cardHeading: string
+  cardLabel: string
+  cardBody: string
+  link: Link
+  image: ImageProps
+}
+
+export interface Text extends Sys {
+  type: "Text"
+  title: string
+  text: string
+}
+
+export interface InternalLink extends Sys {
+  type: "InternalLink"
+  page: LegacyPage
+  title: string
+  linkStyle: string
+}
+
+export interface ExternalLink extends Sys {
+  type: "ExternalLink"
+  url: string
+  title: string
+  linkStyle: string
+}
+export interface Video extends Sys {
+  type: "Video"
+  title: string
+  videoUrl: string
+  transcript: string
+}
+
+export interface Image extends Sys {
+  type: "Image"
+  title: string
+  image: ImageProps
+}
+
+export interface ImageProps {
+  url: string
+  title: string
+  description: string
+  height: number
+  width: number
+}
+
+export type Link = InternalLink | ExternalLink
+
+//export interface TenantPage extends DefaultPage {}
+
+export type TenantPage = LegacyPage | PressReleasePage
+
+// type Page = DefaultPage | LegacyPage | PressReleasePage
+export type TenantContentModule = Section | EditorialCard | Text | Video | Image
